f6af8f50ba6a40655155dc8505225b0eeeefedac
jennakimsd <jennakimsd@gmail.com>
2020-04-12 17:30:04 -0700

    I did Firework and More Array Fun (IP)

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,41 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
 	
-	
-	
+	}
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	public static void printString(String[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	public static void reverseString(String[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	public static void skipString(String[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+			i++;
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	public static void randomString(String[] arr) {
+		Random r = new Random();
+		for(int i = 0; i < arr.length; i++) {
+			
+		}
+	}
 	
 }
